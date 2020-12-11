6b8030ab7421072eb5790ef515929f2e8862dec0
Chris Lagasse <soben@outlook.com>
2020-12-10 22:43:02 -0500

    Background image now supported for mastheads.

--- project/themes/beth-chernes/index.php
+++ project/themes/beth-chernes/index.php
@@ -3,14 +3,14 @@
 $context = Timber\Timber::context();
 $context['posts'] = Timber\Timber::get_posts();
 
-if (is_singular()) {
-  $post = $context['posts'][0];
-  $context['header'] = get_field("masthead_title", $post->ID) ?: $post->title;
-  $context['subheader'] = get_field("masthead_subheader", $post->ID);
-} else if ( is_post_type_archive( "post" ) || is_home() ) {
-  $context['header'] = "Blog"; // Title for posts page
-} else {
-  $context['header'] = "404";
-}
+// if (is_singular()) {
+//   $post = $context['posts'][0];
+//   $context['header'] = get_field("masthead_title", $post->ID) ?: $post->title;
+//   $context['subheader'] = get_field("masthead_subheader", $post->ID);
+// } else if ( is_post_type_archive( "post" ) || is_home() ) {
+//   $context['header'] = "Blog"; // Title for posts page
+// } else {
+//   $context['header'] = "404";
+// }
 
 Timber\Timber::render( 'index.twig', $context );
