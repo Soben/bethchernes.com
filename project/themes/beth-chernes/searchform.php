958d4842ee38f6c61a2fe7d150b530aa7ef00026
Chris Lagasse <soben@outlook.com>
2020-12-18 22:48:18 -0500

    Search form adjustments. Style adjustments for global form elements. Sidebar completion.

---
+++ project/themes/beth-chernes/searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" class="form--search" action="<?php echo site_url(); ?>">
+  <div class="d-flex justify-content-center">
+    <label class="screen-reader-text sr-only" for="s">Search for:</label>
+    <input type="text" value="" name="s" placeholder="Search..." />
+    <input type="submit" value="Search" />
+  </div>
+</form>
