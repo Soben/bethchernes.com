bd42a59297a594e165f99e5937bc61dca14c46d0
Chris Lagasse <soben@outlook.com>
2020-12-19 00:07:58 -0500

    Adjustments to masthead on responsive. Sidebar and Post in progress. meta separated into header and footer and styles adjusted accordingly.

---
+++ project/themes/beth-chernes/single.php
@@ -0,0 +1,7 @@
+<?php
+
+$context = Timber\Timber::context();
+$context['posts'] = new Timber\PostQuery();
+$context['sidebar'] = Timber\Timber::get_sidebar('sidebar.php');
+
+Timber\Timber::render( 'single.twig', $context );
