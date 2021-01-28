1dd65984f61474b54365108c256424ba7e143c27
Chris Lagasse <soben@outlook.com>
2021-01-28 16:24:33 -0500

    #22 Removed wordpress emoji.

--- project/themes/beth-chernes/src/Theme.php
+++ project/themes/beth-chernes/src/Theme.php
@@ -26,6 +26,7 @@ class Theme extends Timber\Site {
 
 		// Actions
 		add_action( "after_setup_theme", [$this, "theme_supports"] );
+		add_action( "init", [$this, "disable_emoji"]);
 		add_action( "init", [$this, "register_post_types"] );
 		add_action( "init", [$this, "register_taxonomies"] );
 		add_action( "init", [$this, "register_acf_fields"] );
@@ -39,7 +40,38 @@ class Theme extends Timber\Site {
 		parent::__construct();
 	}
 
-	function cleanup_archive_title( $title ) {
+	public function disable_emoji ()
+	{
+		remove_action( "wp_head", "print_emoji_detection_script", 7 );
+		remove_action( "admin_print_scripts", "print_emoji_detection_script" );
+		remove_action( "wp_print_styles", "print_emoji_styles" );
+		remove_action( "admin_print_styles", "print_emoji_styles" ); 
+		remove_filter( "the_content_feed", "wp_staticize_emoji" );
+		remove_filter( "comment_text_rss", "wp_staticize_emoji" ); 
+		remove_filter( "wp_mail", "wp_staticize_emoji_for_email" );
+		add_filter( "tiny_mce_plugins", [$this, "disable_emojis_tinymce"] );
+		add_filter( "wp_resource_hints", [$this, "disable_emojis_remove_dns_prefetch"], 10, 2 );
+	}
+
+	public function disable_emojis_tinymce( $plugins ) {
+		if ( is_array( $plugins ) ) {
+			return array_diff( $plugins, array( "wpemoji" ) );
+		} else {
+			return array();
+		}
+	}
+	 
+	public function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+		if ( "dns-prefetch" == $relation_type ) {
+			/** This filter is documented in wp-includes/formatting.php */
+			$emoji_svg_url = apply_filters( "emoji_svg_url", "https://s.w.org/images/core/emoji/2/svg/" );
+			$urls = array_diff( $urls, array( $emoji_svg_url ) );
+		}
+
+		return $urls;
+	}
+
+	public function cleanup_archive_title( $title ) {
     if ( is_category() ) {
         $title = single_cat_title( "", false );
     } elseif ( is_tag() ) {
