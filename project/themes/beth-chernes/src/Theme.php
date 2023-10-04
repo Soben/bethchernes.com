ba706eb01ce45d3aaf154e2b17a1b5222831e302
Chris Lagasse <chrislagasse@protonmail.com>
2023-10-03 21:55:34 -0400

    Bumped the version number.

--- project/themes/beth-chernes/src/Theme.php
+++ project/themes/beth-chernes/src/Theme.php
@@ -8,7 +8,7 @@ use Twig;
 class Theme extends Timber\Site {
  
 	static $THEME_NAME = "beth-chernes";
-	static $VERSION = "2023.07.01";
+	static $VERSION = "2023.10.03";
 	
 	/** Register */
 	public function __construct()
