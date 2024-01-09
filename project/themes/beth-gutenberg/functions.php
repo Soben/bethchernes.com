e6595b77f3ba0da8ea3dbb668b8c5fa97b815d32
Chris Lagasse <chrislagasse@protonmail.com>
2024-01-09 17:26:36 -0500

    General baseline of theme. currently including dist folder until github action can be updated. Base setup of style and script enqueue. Starting migration from old theme.

--- project/themes/beth-gutenberg/functions.php
+++ project/themes/beth-gutenberg/functions.php
@@ -1,3 +1,11 @@
 <?php
 
 require_once( __DIR__ . '/vendor/autoload.php' );
+
+add_theme_support( 'align-wide' );
+
+add_action('wp_enqueue_scripts', function() {
+	// @TODO set version number instead of false.
+	wp_enqueue_style('beth-chernes', get_stylesheet_directory_uri() . '/assets/dist/main.css', [], false, 'all');
+	wp_enqueue_script('beth-chernes', get_stylesheet_directory_uri() . '/assets/dist/main.js', ['jquery'], false, true );
+});
---
+++ project/themes/beth-gutenberg/index.php
@@ -0,0 +1,253 @@
+<!doctype html>
+<html>
+	<head>
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+							
+<!-- Block: H1 -->
+<h1 class="wp-block-heading">Heading 1</h1>
+
+<!-- Block: H2 -->
+<h2 class="wp-block-heading">Heading 2</h2>
+
+<!-- Block: H3 -->
+<h3 class="wp-block-heading">Heading 3</h3>
+
+<!-- Block: H4 -->
+<h4 class="wp-block-heading">Heading 4</h4>
+
+<!-- Block: H5 -->
+<h5 class="wp-block-heading">Heading 5</h5>
+
+<!-- Block: H6 -->
+<h6 class="wp-block-heading">Heading 6</h6>
+
+<!-- Block: Paragraph -->
+<p>left align <sup>superscript</sup> ipsum dolor sit <strong>bold</strong>, consectetur <em>italic</em> elit, sed do eiusmod tempor <a href="https://chrislagasse.com" data-type="link" data-id="https://chrislagasse.com">link somewhere</a> ut labore et <s>strikethrough</s> magna aliqua. Ut enim <code>&lt;?php echo 'inline code'; ?&gt;</code>, quis nostrud <kbd>keyboard input</kbd> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit<sup data-fn="3437d96d-4da1-433e-b24d-ba23da9fc84b" class="fn"><a href="#3437d96d-4da1-433e-b24d-ba23da9fc84b" id="3437d96d-4da1-433e-b24d-ba23da9fc84b-link">1</a></sup> in voluptate velit esse cillum dolore eu fugiat nulla <bdo lang="fr" dir="ltr">language</bdo>. Excepteur <sub>subscript</sub> occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est <mark style="background-color:#c2a990" class="has-inline-color">highlighted text</mark>. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut</p>
+
+<!-- Block: Image Align: Left -->
+<div class="wp-block-image">
+<figure class="alignleft size-full is-resized"><img fetchpriority="high" decoding="async" width="578" height="434" src="http://placekitten.com/578/434" alt="" class="wp-image-3313" style="width:242px;height:auto" sizes="(max-width: 578px) 100vw, 578px" /><figcaption class="wp-element-caption">Align Right with Caption</figcaption></figure></div>
+
+<!-- Block: Paragraph -->
+<p>left align <sup>superscript</sup> ipsum dolor sit <strong>bold</strong>, consectetur <em>italic</em> elit, sed do eiusmod tempor <a href="https://chrislagasse.com" data-type="link" data-id="https://chrislagasse.com">link somewhere</a> ut labore et <s>strikethrough</s> magna aliqua. Ut enim <code>&lt;?php echo 'inline code'; ?&gt;</code>, quis nostrud <kbd>keyboard input</kbd> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit<sup data-fn="3437d96d-4da1-433e-b24d-ba23da9fc84b" class="fn"><a href="#3437d96d-4da1-433e-b24d-ba23da9fc84b" id="3437d96d-4da1-433e-b24d-ba23da9fc84b-link">1</a></sup> in voluptate velit esse cillum dolore eu fugiat nulla <bdo lang="fr" dir="ltr">language</bdo>. Excepteur <sub>subscript</sub> occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est <mark style="background-color:#c2a990" class="has-inline-color">highlighted text</mark>. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut</p>
+
+<!-- Block: Image Align: Right -->
+<div class="wp-block-image">
+<figure class="wp-block-image alignright size-full is-resized"><img decoding="async" width="768" height="1024" src="http://placekitten.com/768/1024" alt="" class="wp-image-3311" style="width:242px;height:auto" sizes="(max-width: 768px) 100vw, 768px" /><figcaption class="wp-element-caption">Align Right with Caption</figcaption></figure></div>
+
+<!-- Block: Paragraph -->
+<p>left align <sup>superscript</sup> ipsum dolor sit <strong>bold</strong>, consectetur <em>italic</em> elit, sed do eiusmod tempor <a href="https://chrislagasse.com" data-type="link" data-id="https://chrislagasse.com">link somewhere</a> ut labore et <s>strikethrough</s> magna aliqua. Ut enim <code>&lt;?php echo 'inline code'; ?&gt;</code>, quis nostrud <kbd>keyboard input</kbd> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit<sup data-fn="3437d96d-4da1-433e-b24d-ba23da9fc84b" class="fn"><a href="#3437d96d-4da1-433e-b24d-ba23da9fc84b" id="3437d96d-4da1-433e-b24d-ba23da9fc84b-link">1</a></sup> in voluptate velit esse cillum dolore eu fugiat nulla <bdo lang="fr" dir="ltr">language</bdo>. Excepteur <sub>subscript</sub> occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est <mark style="background-color:#c2a990" class="has-inline-color">highlighted text</mark>. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut</p>
+
+<!-- Block: Unordered List -->
+<ul>
+	<li>Unordered List</li>
+	<li>Contains several
+		<ul>
+			<li>Items and</li>
+			<li>subitems
+				<ul>
+					<li>three layers deep</li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+</ul>
+
+<!-- Block: Paragraph Align: Center-->
+<p class="has-text-align-center">centered text Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut</p>
+
+<!-- Block: Paragraph with Inline Image -->
+<p>inline image odit aut fugit, sed quia <img decoding="async" width="500" height="500" class="wp-image-3308" style="width: 150px;" src="http://placekitten.com/500/500" alt="" sizes="(max-width: 1184px) 100vw, 1184px" />consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.</p>
+
+<!-- Block: Paragraph Align: Right -->
+<p class="has-text-align-right">right-aligned text Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut</p>
+
+<!-- Block: Ordered List -->
+<ol>
+	<li>Ordered list
+		<ol>
+			<li>Secondary item</li>
+		</ol>
+	</li>
+	<li>List item 2</li>
+</ol>
+
+<!-- Block: Gallery -->
+<figure class="wp-block-gallery has-nested-images columns-default is-cropped wp-block-gallery-2 is-layout-flex wp-block-gallery-is-layout-flex">
+	<figure class="wp-block-image size-large"><img loading="lazy" decoding="async" width="768" height="1024" data-id="3309" src="http://placekitten.com/768/1024" alt="" class="wp-image-3309" sizes="(max-width: 768px) 100vw, 768px" /></figure>
+	<figure class="wp-block-image size-large"><img loading="lazy" decoding="async" width="1024" height="768" data-id="3310" src="http://placekitten.com/1024/768" alt="" class="wp-image-3310" sizes="(max-width: 1024px) 100vw, 1024px" /></figure>
+	<figure class="wp-block-image size-large"><img loading="lazy" decoding="async" width="768" height="1024" data-id="3312" src="http://placekitten.com/768/1024" alt="" class="wp-image-3312" sizes="(max-width: 768px) 100vw, 768px" /></figure>
+	<figcaption class="blocks-gallery-caption wp-element-caption">Gallery Caption</figcaption>
+</figure>
+
+<!-- Block: Block Quote -->
+<blockquote class="wp-block-quote">
+	<p>Quote Block, includes the ability to have images, but we&#8217;re ignoring that for the time being.</p>
+	<cite>Chris Lagasse, Developer</cite>
+</blockquote>
+
+<!-- Block: Image Align: Wide -->
+<figure class="wp-block-image alignwide size-full">
+	<img loading="lazy" decoding="async" width="768" height="1024" src="http://placekitten.com/768/1024" alt="" class="wp-image-3312" sizes="(max-width: 768px) 100vw, 768px" />
+</figure>
+
+<!-- Block: Archive -->
+<ul class="wp-block-archives-list wp-block-archives">	<li><a href='#'>April 2023</a></li>
+	<li><a href='#'>December 2022</a></li>
+	<li><a href='#'>June 2022</a></li>
+	<li><a href='#'>January 2022</a></li>
+	<li><a href='#'>August 2021</a></li>
+	<li><a href='#'>February 2021</a></li>
+	<li><a href='#'>January 2021</a></li>
+	<li><a href='#'>November 2020</a></li>
+	<li><a href='#'>September 2020</a></li>
+	<li><a href='#'>August 2020</a></li>
+	<li><a href='#'>July 2020</a></li>
+	<li><a href='#'>June 2020</a></li>
+	<li><a href='#'>May 2020</a></li>
+	<li><a href='#'>April 2020</a></li>
+</ul>
+
+<!-- Block: Audio -->
+<figure class="wp-block-audio"><audio controls src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"></audio></figure>
+
+<!-- Block: Buttons -->
+<div class="wp-block-buttons is-content-justification-center is-layout-flex wp-container-core-buttons-layout-1 wp-block-buttons-is-layout-flex">
+	<div class="wp-block-button">
+		<a class="wp-block-button__link has-text-align-center wp-element-button">Call to Action Button</a>
+	</div>
+</div>
+
+<!-- Block: Preformatted Code -->
+<pre class="wp-block-code">
+	<code>
+		$context = Timber\Timber::context();
+		$context&#91;'posts'] = new Timber\PostQuery();
+		$context&#91;'sidebar'] = Timber\Timber::get_sidebar('sidebar.php');
+
+		Timber\Timber::render( 'archive.twig', $context );
+	</code>
+</pre>
+
+<!-- Block: Image Align: Full -->
+<figure class="wp-block-image alignfull size-full"><img loading="lazy" decoding="async" width="578" height="434" src="http://placekitten.com/578/434" alt="" class="wp-image-3313" sizes="(max-width: 578px) 100vw, 578px" /></figure>
+
+<!-- Block: Description -->
+<details class="wp-block-details is-layout-flow wp-block-details-is-layout-flow"><summary>Summary/Description</summary>
+	<p>Hidden content here</p>
+</details>
+
+<!-- Block: Preformatted -->
+<pre class="wp-block-preformatted">Preformatted Text Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut </pre>
+
+<!-- Block: Pull Quote -->
+<figure class="wp-block-pullquote">
+	<blockquote><p>Pull Quote</p><cite>WordPress Core Block</cite></blockquote>
+</figure>
+
+<!-- Block: Table -->
+<figure class="wp-block-table">
+	<table><tbody><tr><td>Fancy Table</td><td>Column 2</td></tr><tr><td>Check this out</td><td></td></tr></tbody></table>
+	<figcaption class="wp-element-caption">Table Caption</figcaption>
+</figure>
+
+<!-- Block: Verse -->
+<pre class="wp-block-verse">
+	Two roads diverged in a yellow wood,
+	And sorry I could not travel both
+	And be one traveler, long I stood
+	And looked down one as far as I could
+	To where it bent in the undergrowth;
+
+	Then took the other, as just as fair,
+	And having perhaps the better claim,
+	Because it was grassy and wanted wear;
+	Though as for that the passing there
+	Had worn them really about the same,
+
+	And both that morning equally lay
+	In leaves no step had trodden black.
+	Oh, I kept the first for another day!
+	Yet knowing how way leads on to way,
+	I doubted if I should ever come back.
+</pre>
+
+<!-- Block: Cover -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-right">
+	<span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span>
+	<img loading="lazy" decoding="async" width="578" height="434" class="wp-block-cover__image-background wp-image-3313" alt="" src="http://placekitten.com/578/434" data-object-fit="cover" sizes="(max-width: 578px) 100vw, 578px" />
+	<div class="wp-block-cover__inner-container is-layout-constrained wp-block-cover-is-layout-constrained">
+		<p class="has-text-align-center has-large-font-size">Chapter Block</p>
+	</div>
+</div>
+
+<!-- Block: File -->
+<div class="wp-block-file"><a id="wp-block-file--media-1c55ad0f-309b-4ffb-b0cc-b317a30e124f" href="http://placekitten.com/768/1024">img_1656097920333304_photo</a><a href="http://placekitten.com/768/1024" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-1c55ad0f-309b-4ffb-b0cc-b317a30e124f">Download</a></div>
+
+<!-- Block: Horizontal Group -->
+<div class="wp-block-group is-nowrap is-layout-flex wp-container-core-group-layout-6 wp-block-group-is-layout-flex">
+	<p>Horizontal Block #1</p>
+	<p>Horizontal Block #2</p>
+</div>
+
+<!-- Block: Read More -->
+<span id="more-3305"></span>
+
+<!-- Block: Columns -->
+<div class="wp-block-columns is-layout-flex wp-container-core-columns-layout-1 wp-block-columns-is-layout-flex">
+	<div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
+		<h2 class="wp-block-heading">Column #1</h2>
+	</div>
+
+	<div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
+		<p>Two roads diverged in a yellow wood,<br>And sorry I could not travel both<br>And be one traveler, long I stood<br>And looked down one as far as I could<br>To where it bent in the undergrowth;</p>
+	</div>
+
+
+
+	<div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow">
+		<ul>
+			<li>Column #3</li>
+			<li>Content #2</li>
+		</ul>
+	</div>
+</div>
+
+<!-- Block: Youtube Embed -->
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+	<div class="wp-block-embed__wrapper">
+		<iframe loading="lazy" title="Realistic-looking &#039;Star Wars&#039; lightsaber unveiled at SXSW event #Shorts" width="500" height="281" src="https://www.youtube.com/embed/sCqfGqexco4?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+	</div>
+</figure>
+
+<!-- Block: Media and Text -->
+<div class="wp-block-media-text is-stacked-on-mobile">
+	<figure class="wp-block-media-text__media">
+		<img loading="lazy" decoding="async" width="1024" height="768" src="http://placekitten.com/1024/768" alt="" class="wp-image-3310 size-full" sizes="(max-width: 1024px) 100vw, 1024px" />
+	</figure>
+	<div class="wp-block-media-text__content">
+		<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
+
+<!-- Block: Spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+
+<!-- Block: Divider/Separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
+
+<!-- Block: Paragraph -->
+<p><strong>spacer block above the divider</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<!-- Block: Footnotes -->
+<ol class="wp-block-footnotes">
+	<li id="3437d96d-4da1-433e-b24d-ba23da9fc84b">Footnote <a href="#3437d96d-4da1-433e-b24d-ba23da9fc84b-link" aria-label="Jump to footnote reference 1">↩︎</a></li>
+	<li id="3437d96d-4da1-433e-b24d-ba23da9fc84b">Footnote <a href="#3437d96d-4da1-433e-b24d-ba23da9fc84b-link" aria-label="Jump to footnote reference 2">↩︎</a></li>
+	<li id="3437d96d-4da1-433e-b24d-ba23da9fc84b">Footnote <a href="#3437d96d-4da1-433e-b24d-ba23da9fc84b-link" aria-label="Jump to footnote reference 3">↩︎</a></li>
+</ol>
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
